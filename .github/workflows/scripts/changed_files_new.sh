61adbc314febaf2d787b23e9a3b4bb04ab2c08e4
Randall Reed, Jr <rcrjr@hey.com>
2020-07-30 14:31:32 -0400

    Extract set-output from script

---
+++ .github/workflows/scripts/changed_files_new.sh
@@ -0,0 +1,1 @@
+git diff --name-only `git merge-base origin/${GITHUB_BASE_REF} origin/${GITHUB_HEAD_REF}`..origin/${GITHUB_HEAD_REF} "**/*/*.sh" | awk -F '/' '{ print $NF }' | sed "s/\.sh$/${children}/g" | sed 's/^/+/g' | tr '\n' ' '
